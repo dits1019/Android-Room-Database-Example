100021118209ced080dede2e531053d218acdadf
dits1019 <jdits1019@gmail.com>
2021-09-10 10:43:39 +0900

    first commit

---
+++ app/src/main/java/com/example/androidroomdatabaseex/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidroomdatabaseex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
